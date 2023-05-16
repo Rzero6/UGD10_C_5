8fe5ed7127b4ed648f2b25fd8abd2e7e9e5797db
Reynoldssss <michaelreynoldk@gmail.com>
2023-05-16 14:21:06 +0700

    Merge origin/master

--- src/view/PenyewaanView.java
+++ src/view/PenyewaanView.java
@@ -70,7 +70,7 @@ public class PenyewaanView extends javax.swing.JFrame {
         kendaraanViewBtn = new javax.swing.JButton();
         penyewaanBtn = new javax.swing.JButton();
         customerViewBtn = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        titleLabel = new javax.swing.JLabel();
         actionBar = new javax.swing.JPanel();
         addBtn = new javax.swing.JButton();
         editBtn = new javax.swing.JButton();
@@ -197,11 +197,11 @@ public class PenyewaanView extends javax.swing.JFrame {
             }
         });
 
-        jLabel1.setFont(new java.awt.Font("Bauhaus 93", 3, 48)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(51, 51, 51));
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/horse.png"))); // NOI18N
-        jLabel1.setText("LALARENT ");
-        jLabel1.setBorder(javax.swing.BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        titleLabel.setFont(new java.awt.Font("Bauhaus 93", 3, 48)); // NOI18N
+        titleLabel.setForeground(new java.awt.Color(51, 51, 51));
+        titleLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/horse.png"))); // NOI18N
+        titleLabel.setText("LALARENT ");
+        titleLabel.setBorder(javax.swing.BorderFactory.createEmptyBorder(5, 5, 5, 5));
 
         javax.swing.GroupLayout headerLayout = new javax.swing.GroupLayout(header);
         header.setLayout(headerLayout);
@@ -215,7 +215,7 @@ public class PenyewaanView extends javax.swing.JFrame {
                 .addGap(35, 35, 35)
                 .addComponent(customerViewBtn)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel1)
+                .addComponent(titleLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(kelompokPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(21, 21, 21))
@@ -233,7 +233,7 @@ public class PenyewaanView extends javax.swing.JFrame {
                     .addComponent(kendaraanViewBtn)
                     .addGroup(headerLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                         .addComponent(kelompokPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(titleLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(28, 28, 28))
         );
 
@@ -791,7 +791,6 @@ public class PenyewaanView extends javax.swing.JFrame {
     private javax.swing.JLabel hargaLabel;
     private javax.swing.JPanel header;
     private javax.swing.JPanel inputPanel;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel kelompokLabel;
@@ -812,6 +811,7 @@ public class PenyewaanView extends javax.swing.JFrame {
     private javax.swing.JPanel tablePanel;
     private javax.swing.JTable tablePenyewaan;
     private javax.swing.JCheckBox tissueCheck;
+    private javax.swing.JLabel titleLabel;
     // End of variables declaration//GEN-END:variables
 
     public void setComponent(boolean b) {
